41ce6890871dc963b33553065c164d692b0108dc
AlexChampi <89869751+AlexChampi@users.noreply.github.com>
2022-03-17 13:30:00 +0300

    Delete src/main/kotlin/ru/tinkof/fintech/kotlin/collections directory

--- src/main/kotlin/ru/tinkof/fintech/kotlin/collections/Queue.kt
+++
@@ -1,8 +0,0 @@
-package ru.tinkof.fintech.kotlin.collections
-
-interface Queue<T> {
-    fun enqueue(element: Any)
-    fun dequeue()
-    fun getTop(): Any
-    fun isEmpty(): Boolean
-}
--- src/main/kotlin/ru/tinkof/fintech/kotlin/collections/Stack.kt
+++
@@ -1,8 +0,0 @@
-package ru.tinkof.fintech.kotlin.collections
-
-interface Stack<T> {
-    fun push(element: T)
-    fun pop(): Any?
-    fun peek(): Any?
-    fun isEmpty(): Boolean
-}
