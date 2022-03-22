92bf4dcfa4c94587e5b490de6bb63038f3c85001
AlexChamp <sasha.g27204@gmail.com>
2022-03-22 15:03:48 +0300

    change convertor

--- src/main/kotlin/ru/tinkoff/fintech/hw5/Convertor.kt
+++ src/main/kotlin/ru/tinkoff/fintech/hw5/Convertor.kt
@@ -3,12 +3,6 @@ package ru.tinkoff.fintech.hw5
 class Convertor(private val dictionary: Dictionary, private val rate: Int = 100) {
 
     fun convert(automobile: Automobile): Automobile =
-        Automobile(
-            dictionary.getWord(automobile.model),
-            automobile.make,
-            automobile.body,
-            automobile.price / rate,
-            automobile.gasMileage,
-            automobile.year
-        )
+        automobile.copy(model = dictionary.getWord(automobile.model), price = automobile.price / rate)
+
 }
