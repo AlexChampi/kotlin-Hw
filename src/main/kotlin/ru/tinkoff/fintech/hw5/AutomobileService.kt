de636d5d3afd6c28a3134fff582e45fa81312f0e
AlexChamp <sasha.g27204@gmail.com>
2022-03-22 19:40:57 +0300

    change convertor

--- src/main/kotlin/ru/tinkoff/fintech/hw5/AutomobileService.kt
+++ src/main/kotlin/ru/tinkoff/fintech/hw5/AutomobileService.kt
@@ -8,8 +8,8 @@ class AutomobileService(private val convertor: Convertor) {
 
     fun bodyGrouping(list: List<Automobile>): Map<String, List<Automobile>> = list.groupBy { it.body }
 
-    fun yearFiltration(list: List<Automobile>, maxYear: Int): List<String> =
-        list.asSequence().sortedBy { it.year }.filter { it.year >= maxYear }
+    fun yearFiltration(list: List<Automobile>, minYear: Int): List<String> =
+        list.asSequence().sortedBy { it.year }.filter { it.year >= minYear }
             .map { "${it.model} ${it.make} was assembled in ${it.year}" }
             .take(3).toList()
 }
--- src/test/kotlin/AutomobileServiceTests.kt
+++ src/test/kotlin/AutomobileServiceTests.kt
@@ -3,18 +3,22 @@ import org.junit.jupiter.api.Test
 import ru.tinkoff.fintech.hw5.*
 
 class AutomobileServiceTests {
+    var dictionary = Dictionary(DICTIONARY_MAP)
+    val convertor = Convertor(dictionary, rate)
+    val automobileService = AutomobileService(convertor)
+
 
     @Test
     fun internationFormat() {
 
-        val result = automobileService.internationalFormatSort(list)
+        val result = automobileService.internationalFormatSort(autos)
 
         val expectedResult = listOf(
-            auto4.copy(model = dictionary.getValue(auto4.model), price = auto4.price / rate),
-            auto5.copy(model = dictionary.getValue(auto5.model), price = auto5.price / rate),
-            auto1.copy(model = dictionary.getValue(auto1.model), price = auto1.price / rate),
-            auto3.copy(model = dictionary.getValue(auto3.model), price = auto3.price / rate),
-            auto2.copy(model = dictionary.getValue(auto2.model), price = auto2.price / rate)
+            auto4.copy(model = DICTIONARY_MAP.getValue(auto4.model), price = auto4.price / rate),
+            auto5.copy(model = DICTIONARY_MAP.getValue(auto5.model), price = auto5.price / rate),
+            auto1.copy(model = DICTIONARY_MAP.getValue(auto1.model), price = auto1.price / rate),
+            auto3.copy(model = DICTIONARY_MAP.getValue(auto3.model), price = auto3.price / rate),
+            auto2.copy(model = DICTIONARY_MAP.getValue(auto2.model), price = auto2.price / rate)
         )
         assertEquals(expectedResult, result)
 
@@ -24,7 +28,7 @@ class AutomobileServiceTests {
     fun grouping() {
 
 
-        val result = automobileService.bodyGrouping(list)
+        val result = automobileService.bodyGrouping(autos)
 
         val expectedResult = mapOf(
             "SUV" to listOf(auto1),
@@ -39,7 +43,7 @@ class AutomobileServiceTests {
     fun filtration() {
 
 
-        val result = automobileService.yearFiltration(list, 2018)
+        val result = automobileService.yearFiltration(autos, 2018)
 
         val expectedResult = listOf(
             "Мини Countryman was assembled in 2018",
@@ -54,17 +58,15 @@ class AutomobileServiceTests {
     val auto3 = Automobile("БМВ", "M5", "Coupe", 7_000_000, 9, 2019)
     val auto4 = Automobile("Лада", "XRay", "CUV", 1_200_000, 10, 2020)
     val auto5 = Automobile("Мини", "Countryman", "Hatchback", 3_000_000, 6, 2018)
-    val list = listOf(auto1, auto2, auto3, auto4, auto5)
-    private val dictionary = mapOf(
-        "Форд" to "Ford",
-        "Бентли" to "Bentley",
-        "БМВ" to "BMW",
-        "Лада" to "Lada",
-        "Мини" to "Mini"
-    )
-    val dict = Dictionary(dictionary)
-    val rate = 110
-    val convertor = Convertor(dict, rate)
-    val automobileService = AutomobileService(convertor)
+    val autos = listOf(auto1, auto2, auto3, auto4, auto5)
 
 }
+
+private const val rate = 110
+private val DICTIONARY_MAP = mapOf(
+    "Форд" to "Ford",
+    "Бентли" to "Bentley",
+    "БМВ" to "BMW",
+    "Лада" to "Lada",
+    "Мини" to "Mini"
+)
